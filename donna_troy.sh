105aad29bc21d5e7585248dd6d7a6f8c0c782f9c
Cleiton <cleiton.silvati@gmail.com>
2019-08-31 15:08:11 +0000

    teteste

--- donna_troy.sh
+++ donna_troy.sh
@@ -5,5 +5,13 @@ do
 figlet $batata
 done
 
+for COWSAY in $(ls /usr/share/cowsay/cows/)
+do
+	sleep 1;
+	cowsay -f $COWSAY LINUX
+	clear;
+done
+
 for i in $(seq 1 25); do linuxlogo -L $i ; sleep 1; clear;  done 
+
 
